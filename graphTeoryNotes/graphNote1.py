d600370e9ee0038105837bee457d4f12ca56499e
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-03-30 08:08:49 +0200

    add: test with networkx

---
+++ graphTeoryNotes/graphNote1.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+
+
+
+def plotCompleteGraph(n):
+    """ Plots a complete graph of n nodes"""
+    G = nx.complete_graph(n)
+    nx.draw_spring(G)
+    plt.show()
+
+
+
+plotCompleteGraph(4)
