060c04656fbe4b9b1f45fb1801546dcf9dda796c
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-04-05 21:04:40 +0200

    add: graph method

--- graphTeoryNotes/graphNote1.py
+++ graphTeoryNotes/graphNote1.py
@@ -12,5 +12,16 @@ def plotCompleteGraph(n):
     plt.show()
 
 
+def drawRandom():
+    G = nx.Graph()
+    G.add_edge('A', 'B', weight=4)
+    G.add_edge('B', 'D', weight=2)
+    G.add_edge('A', 'C', weight=3)
+    G.add_edge('C', 'D', weight=4)
 
-plotCompleteGraph(20)
+    nx.draw_networkx(G)
+    print(nx.shortest_path(G, 'A', 'D', weight='weight'))
+    plt.show()
+
+
+drawRandom()
